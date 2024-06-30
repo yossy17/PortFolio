4f730bb76ef5d088c159a87216ecef3f8e58c73b
yocci_ii <hiitoy0117@gmail.com>
2024-07-01 04:53:30 +0900

    commit

--- components/Base/Header/HeaderMain/HeaderMain.tsx
+++ components/Base/Header/HeaderMain/HeaderMain.tsx
@@ -15,7 +15,6 @@ const HeaderMain = () => {
   return (
     <div className='header__main'>
       <Link href='/' className='header__main__icon'>
-        {/* <Image src='/Imgs/Yossy.webp' alt='icon' width={56} height={56} unoptimized></Image> */}
         <h1 className={`header__main__icon__name ${poppins.className}`}>
           <span className='header__main__icon__name--normal'>Yo</span>
           <span className='header__main__icon__name--change'>s_s</span>
@@ -25,7 +24,6 @@ const HeaderMain = () => {
       <div className='header__main__search'>
         <form>
           <input className='header__main__search__input' placeholder='Search' />
-          {/* <button className='header__main__search__button' type='button'></button> */}
         </form>
       </div>
     </div>
