465f7ffb834581a02163e4b3110fac27a1b56f2c
yocci_ii <hiitoy0117@gmail.com>
2024-07-02 02:35:32 +0900

    commit

---
+++ app/loading.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const loading = () => {
+  return (
+    // <div className='loading'>
+    //   <div id='loading__circle--wrap'>
+    //     <span className='loading__circle'></span>
+    //     <span className='loading__circle'></span>
+    //     <span className='loading__circle'></span>
+    //   </div>
+    //   <div id='loading__shadow--wrap'>
+    //     <span className='loading__shadow'></span>
+    //     <span className='loading__shadow'></span>
+    //     <span className='loading__shadow'></span>
+    //   </div>
+    // </div>
+    <></>
+  );
+};
+
+export default loading;
